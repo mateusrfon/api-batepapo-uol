9a1b0b8abc3098000c372bb6be562441dddfa9dc
Mateus Ribeiro <mateusrfon@gmail.com>
2021-06-10 17:10:50 -0300

    initial files

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+import fs from 'fs';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+
+app.listen(4000, () => {
+    console.log("On business baby");
+})
