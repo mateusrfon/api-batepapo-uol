49e846986b47684de73413e516e16d55a867dbfd
Mateus Ribeiro <mateusrfon@gmail.com>
2021-06-10 17:35:43 -0300

    post /participants

--- src/app.js
+++ src/app.js
@@ -1,12 +1,33 @@
 import express from 'express';
 import cors from 'cors';
 import fs from 'fs';
+import dayjs from 'dayjs';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+const participants = [];
+const messages = [];
 
+app.post("/participants", (req, res) => {
+    const { name } = req.body;
+    if (name === '') {
+        res.sendStatus(400);
+    } else if (participants.filter(e => e.name === name).length > 0) {
+        res.sendStatus(401);
+    } else {
+        participants.push({ name, lastStatus: Date.now() });
+        messages.push({ 
+                        from: name, 
+                        to: 'Todos', 
+                        text: 'entra na sala...', 
+                        type: 'status',
+                        time: dayjs().format('HH:mm:ss')
+                    });
+        res.sendStatus(200);
+    };
+});
 
 app.listen(4000, () => {
     console.log("On business baby");
