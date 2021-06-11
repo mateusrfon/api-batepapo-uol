e91299eebf1ff013ac7c17e2b3b7a1430f4513dc
Mateus Ribeiro <mateusrfon@gmail.com>
2021-06-10 21:02:53 -0300

    post message

--- src/app.js
+++ src/app.js
@@ -33,6 +33,26 @@ app.get("/participants", (req, res) => {
     res.send(participants);
 })
 
+app.post("/messages", (req, res) => {
+    const { to, text, type } = req.body;
+    const time = dayjs().format('HH:mm:ss');
+    const from = req.headers.get('User');
+    const message = {
+        from,
+        to,
+        text,
+        type,
+        time
+    };
+    
+    if ((to === '' || text === '') || (type !== 'message' && type !== 'private_message') || !participants.reduce((acc,e) => e.from === from ? acc = true : null, false)) {
+        res.sendStatus(400);
+    } else {
+        messages.push(message);
+        res.sendStatus(200);
+    }
+});
+
 app.listen(4000, () => {
     console.log("On business baby");
 })
